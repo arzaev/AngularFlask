979cd415f20f75ed7e97e42f71913cbf7b2aa8c3
arzaev <email@arzaev.com>
2021-08-10 09:20:01 -0400

    angular build

--- angular-flask/src/app/app.module.ts
+++ angular-flask/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
